14263ba2e3f00bcc1e003259775aea81e5d45cf0
Tyler <twwaneka@gmail.com>
2014-04-17 16:14:37 -0700

    establishes test skeleton
    - two tests for basic routes

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var http = require('http');
 var path = require('path');
 
 var app = express();
+module.exports = app
 
 // all environments
 app.set('port', process.env.PORT || 3000);
---
+++ test/spec.js
@@ -0,0 +1,20 @@
+var should = require('chai').should()
+  , request = require('supertest')
+  , app = require('../app')
+
+
+describe("get '/'", function() {
+  it('should exist', function(done) {
+    request(app)
+      .get('/')
+      .expect(200, done)
+  })
+})
+
+describe("get '/users/:id", function() {
+  it('should exist', function(done) {
+    request(app)
+      .get('/users/1')
+      .expect(200, done)
+  })
+})
